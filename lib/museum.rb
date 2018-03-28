dd24345aad3b32e2fe353f0c070325b859c0fcc9
Cole Hart <cole@colehart.com>
2018-03-28 09:40:59 -0600

    created new class Museum, lib doc and test doc, using Patron class documents as template

---
+++ lib/museum.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Museum
+  attr_reader :name
+              #:exhibits
+
+  def initialize(name)
+    @name = name
+    #@exhibits = []
+  end
+
+  # def add_exhibit(new_exhibit, cost)
+  #   @exhibit << new_exhibit
+  # end
+end
---
+++ test/museum_test.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/museum'
+require 'pry'
+
+class MuseumTest < MiniTest::Test
+
+  def test_it_exists
+    museum = Museum.new(name)
+    assert_instance_of Museum, museum
+  end
+
+  def test_it_requires_name_argument_to_instantiate
+    skip
+    museum = Museum.new("Bob")
+    actual = "Bob"
+    expected = museum.name
+    assert_equal actual, expected
+  end
+
+  def test_it_instantiates_with_empty_interest_array
+    skip
+    museum = Museum.new("Bob")
+    actual = []
+    expected = museum.interests
+    assert_equal actual, expected
+  end
+
+  def test_it_can_add_interests
+    skip
+    museum = Museum.new("Bob")
+    actual = []
+    expected = museum.add_interest("Dead Sea Scrolls")
+    refute_equal actual, expected
+  end
+
+end
